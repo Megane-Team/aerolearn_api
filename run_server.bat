cc65819e702bcc5b765042a3b9b5cf8ff6068c94
Raffi Fabiansyah <133511291+Raffi14@users.noreply.github.com>
2025-01-31 21:28:46 +0700

    Delete run_server.bat

--- run_server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr "IPv4 Address"') do set IP=%%A
-set IP=%IP:~1%
-rem Ganti nilai WEB_URL dalam .env
-powershell -Command "(Get-Content .env) -replace 'WEB_URL=.*', 'WEB_URL=http://%IP%:8000' | Set-Content .env"
-pnpm dev
